dba66d8f23f4de88b6de9591758c47ba1f060447
isaquesb <isaquesb@gmail.com>
2019-06-03 12:03:12 -0300

    PHP 5.6

---
+++ docker/up-php-56.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/docker-compose -f docker/configs/php-56.yml up -d
---
+++ docker/up-rebuild.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+/usr/bin/docker-compose -f docker/configs/redis.yml -f docker/configs/docker-compose-sql-server.yml up -d --build
